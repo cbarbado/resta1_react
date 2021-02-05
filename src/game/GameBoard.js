01816851d8ebf6803c7d1e3642911f1e1abfed84
Cesar Barbado <barbado@barbado.com.br>
2021-02-04 21:59:18 -0300

    move refactor + undo & redo fix

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -22,19 +22,20 @@ class GameBoard {
 
     restoreBoard(board) {
         this.board = board.slice();
-        return [...this.board];
     }
 
     saveBoard(saveArray) {
         saveArray.push(this.board.slice());
     }
 
-    resetBoard() {
-        this.board = GameBoard.BOARD_LAYOUT.slice();
+    resetBoard() { // reducer
+        this.restoreBoard(GameBoard.BOARD_LAYOUT.slice());
+        this.undoHistory = [];
+        this.redoHistory = [];
         return [...this.board];
     }
 
-    pick(i) {
+    pick(i) { // reducer
         if(this.board[i] === 2) {
             this.board[i] = 3;
             this.pickPos  = i;
@@ -45,7 +46,7 @@ class GameBoard {
         return [...this.board];
     }
 
-    undoMove() {
+    undoMove() { // reducer
         if(this.undoHistory.length > 0) {
             this.saveBoard(this.redoHistory);
             this.restoreBoard(this.undoHistory.pop());
@@ -53,7 +54,7 @@ class GameBoard {
         return [...this.board];
     }
 
-    redoMove() {
+    redoMove() { // reducer
         if(this.redoHistory.length > 0) {
             this.saveBoard(this.undoHistory);
             this.restoreBoard(this.redoHistory.pop());
@@ -61,68 +62,69 @@ class GameBoard {
         return [...this.board];
     }
 
-    validMove (dropPos) {
-        if(this.board[dropPos] === 1) { // target tile is free
-            if(Math.floor(this.pickPos / GameBoard.BOARD_SIZE) === Math.floor(dropPos / GameBoard.BOARD_SIZE)) { // HORIZONTAL MOVE
-                if(dropPos - this.pickPos === 2) { // moving 2 tiles to the right
-                    if(this.board[this.pickPos + 1] === 2) { // middle tile is filled
-                        return true;
-                    }
-                }
-                if(dropPos - this.pickPos === -2) {// moving 2 tiles to the left
-                    if(this.board[this.pickPos - 1] === 2) { // middle tile is filled
-                        return true;
-                    }
-                }
-            }
-            if((this.pickPos % GameBoard.BOARD_SIZE) === (dropPos % GameBoard.BOARD_SIZE)) { // HORIZONTAL MOVE
-                if(dropPos - this.pickPos === (2 * GameBoard.BOARD_SIZE)) { // moving 2 tiles down
-                    if(this.board[this.pickPos + GameBoard.BOARD_SIZE] === 2) { // middle tile is filled
-                        return true;
-                    }
-                }
-                if(dropPos - this.pickPos === (-2 * GameBoard.BOARD_SIZE)) {// moving 2 tiles up
-                    if(this.board[this.pickPos - GameBoard.BOARD_SIZE] === 2) { // middle tile is filled
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    
     unpick() {
         this.board[this.pickPos] = 2;
     }
 
-    release() {
+    release() { // reducer
         if(this.pickPos !== null) {
             this.board[this.pickPos] = 2;
         }
         return [...this.board];
     }
 
-    move(dropPos) {
-        if(this.validMove(dropPos)) {
+    checkMove (dropPos) {
+        if(this.board[dropPos] === 1) { // target tile is free
+            if(Math.floor(this.pickPos / GameBoard.BOARD_SIZE) === Math.floor(dropPos / GameBoard.BOARD_SIZE)) { // HORIZONTAL MOVE
+                if(dropPos - this.pickPos === 2) { // moving 2 tiles to the right
+                    if(this.board[this.pickPos + 1] === 2) { // middle tile is filled
+                        return 1; // MOVE RIGHT
+                    }
+                }
+                if(dropPos - this.pickPos === -2) {// moving 2 tiles to the left
+                    if(this.board[this.pickPos - 1] === 2) { // middle tile is filled
+                        return 2; // MOVE LEFT
+                    }
+                }
+            }
+            if((this.pickPos % GameBoard.BOARD_SIZE) === (dropPos % GameBoard.BOARD_SIZE)) { // VERTICAL MOVE
+                if(dropPos - this.pickPos === (2 * GameBoard.BOARD_SIZE)) { // moving 2 tiles down
+                    if(this.board[this.pickPos + GameBoard.BOARD_SIZE] === 2) { // middle tile is filled
+                        return 3; // MOVE DOWN
+                    }
+                }
+                if(dropPos - this.pickPos === (-2 * GameBoard.BOARD_SIZE)) {// moving 2 tiles up
+                    if(this.board[this.pickPos - GameBoard.BOARD_SIZE] === 2) { // middle tile is filled
+                        return 4; // MOVE UP
+                    }
+                }
+            }
+        }
+        return 0; // INVALID MOVE
+    }
+    
+    move(dropPos) { // reducer
+        var moveDir = this.checkMove(dropPos);
+        if(moveDir !== 0) { // VALID MOVE
             this.board[this.pickPos] = 2;
             this.saveBoard(this.undoHistory);
             this.board[dropPos] = 2;
             this.board[this.pickPos] = 1;
-            if(Math.floor(this.pickPos / GameBoard.BOARD_SIZE) === Math.floor(dropPos / GameBoard.BOARD_SIZE)) { // HORIZONTAL MOVE
-                if(dropPos - this.pickPos === 2) { // moving right
+            switch (moveDir) {
+                case 1: // RIGHT
                     this.board[this.pickPos + 1] = 1;
-                }
-                else {
+                    break;
+                case 2: // LEFT
                     this.board[this.pickPos - 1] = 1;
-                }
-            }
-            else { // VERTICAL MOVE
-                if(dropPos - this.pickPos === (2 * GameBoard.BOARD_SIZE)) { // moving 2 tiles down
+                    break;
+                case 3: // DOWN
                     this.board[this.pickPos + GameBoard.BOARD_SIZE] = 1;
-                }
-                else {
+                    break;
+                case 4: // UP
                     this.board[this.pickPos - GameBoard.BOARD_SIZE] = 1;
-                }
+                    break;
+                default:
+                    break;
             }
         }
         else {
